bab29732580ef585b8018baedb4e9819050d136f
Yu <68062347+hille2@users.noreply.github.com>
2021-06-07 14:00:22 +0300

    Delete create_new_repos.sh

--- create_new_repos.sh
+++
@@ -1,7 +0,0 @@
-echo "# 42cursus" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/hille2/42cursus.git
-git push -u origin main
